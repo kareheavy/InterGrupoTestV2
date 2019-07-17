f996ec5d6eccd7f3b6d67c04e63710ebac9ba496
jhon jimenez <jhonsnow73@gmail.com>
2019-07-17 18:03:37 -0500

    commit

--- app/src/main/java/com/jhonjimenez/intergrupotestv2/LoginActivity.kt
+++ app/src/main/java/com/jhonjimenez/intergrupotestv2/LoginActivity.kt
@@ -1,9 +1,9 @@
 package com.jhonjimenez.intergrupotestv2
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
